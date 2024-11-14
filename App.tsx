74eb498272acc64d2385807a12f6ff872581c94f
Opeyemi Anjoorin <anjoorinpaul@gmail.com>
2024-11-14 01:00:27 +0100

    - move project to intro - clear proj

--- App.tsx
+++ App.tsx
@@ -1,29 +1,12 @@
 import React from 'react';
 import { createStaticNavigation } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import ListScreen from './src/ListScreen';
 import HomeScreen from './src/HomeScreen';
-import ComponentsScreen from './src/ComponentsScreen'
-import ImageScreen from './src/ImageScreen';
-import CounterScreen from './src/CounterScreen';
-import ColorScreen from './src/ColorScreen';
-import SquareScreen from './src/SquareScreen';
-import TextScreen from './src/TextScreen';
-import BoxScreen from './src/BoxScreen';
 
 const RootStack = createNativeStackNavigator({
   initialRouteName: 'Home',
   screens: {
-    Home: HomeScreen,
-    List: ListScreen,
-    Components: ComponentsScreen,
-    Image: ImageScreen,
-    Counter: CounterScreen,
-    Color: ColorScreen,
-    Square: SquareScreen,
-    Text:TextScreen,
-    Box: BoxScreen
-
+    Home: HomeScreen
   },
 });
 
--- src/HomeScreen.js
+++ src/HomeScreen.js
@@ -8,38 +8,6 @@ const HomeScreen = props => {
   return (
     <View style={{ flex: 1 }}>
       <Text style={styles.subText}>Hi There</Text>
-      <Button
-        onPress={() => navigation.navigate('Components')}
-        title='Go to Components Demo'
-      />
-      <Button
-        onPress={() => navigation.navigate('List')}
-        title='Go to List Demo'
-      />
-      <Button
-        onPress={() => navigation.navigate('Image')}
-        title='Go to Image Demo'
-      />
-      <Button
-        onPress={() => navigation.navigate('Counter')}
-        title='Go to Counter Demo'
-      />
-      <Button
-        onPress={() => navigation.navigate('Color')}
-        title='Go to Color Demo'
-      />
-      <Button
-        onPress={() => navigation.navigate('Square')}
-        title='Go to Square Demo'
-      />
-      <Button
-        onPress={() => navigation.navigate('Text')}
-        title='Go to Text Demo'
-      />
-      <Button
-        onPress={() => navigation.navigate('Box')}
-        title='Go to Box Demo'
-      />
     </View>
   )
 };
