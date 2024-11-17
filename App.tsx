1aa0d5858f55008cb36775c980a75c9fda7c8e00
Opeyemi Anjoorin <anjoorinpaul@gmail.com>
2024-11-17 18:06:10 +0100

    new

--- App.tsx
+++ App.tsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import { createStaticNavigation } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import ResultsShowScreen from './src/screens/ResultsShowScreen';
-import SearchScreen from './src/screens/SearchScreen';
 
 const RootStack = createNativeStackNavigator({
   initialRouteName: 'Search',
